2b71711a7f1355370dc9a03929ab8c59610bfab2
Sacha Sedriks <sachased@gmail.com>
2020-05-14 20:18:28 -0700

    created 'state-drills' directory and finished Drill 1 <HelloWorld />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import HelloWorld from './state-drills/HelloWorld';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
+      <div>
+        <HelloWorld />
+      </div>
+
+
     </div>
   );
 }
---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      who: 'who?',
+    }
+  }
+  
+  handleButtonClick = (event) => {
+    this.setState(
+      { who: event.target.value }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello, {this.state.who}</p>
+        <button value="world!" onClick={this.handleButtonClick}>World</button>
+        <button value="friend!" onClick={this.handleButtonClick}>Friend</button>
+        <button value="React!" onClick={this.handleButtonClick}>React</button>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld;
