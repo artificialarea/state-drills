16d1e32d5100ec07d303ae1dc30a97c3385689ca
Alen Diaz <noricual@gmail.com>
2020-05-14 22:46:34 -0500

    tikTokBoom Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import HelloWorld from './state-drills/HelloWorld';
-import './App.css';
+import React from "react";
+import HelloWorld from "./state-drills/HelloWorld";
+import "./App.css";
+import Bomb from "./state-drills/Bomb";
 
 function App() {
   return (
     <div className="App">
-      
       <div>
         <HelloWorld />
       </div>
-
-
+      <div>
+        <Bomb />
+      </div>
     </div>
   );
 }
---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class Bomb extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            counter: 0
+        }
+    }
+    componentDidMount(){
+      this.interval =  setInterval(() => {
+            let newCounter = this.state.counter + 1
+            this.setState({
+                counter : newCounter
+            })
+        }, 1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval)
+
+    }
+
+    render(){
+
+        let sound = 'sound'
+        if (this.state.counter % 2 === 0){
+            sound = 'Tik'
+        }
+        else{
+            sound = 'Tok'
+        }
+        if(this.state.counter === 10){
+            sound = 'BooM'
+            this.componentWillUnmount()
+        }
+
+        return(
+            <div>
+                <p>{sound}</p>
+                <p>{this.state.counter}</p>
+            </div>
+        )
+    }
+}
+
+export default Bomb;
