7deb1a9d0e8b192977da6cdf65114235704d2e45
Sacha Sedriks <sachased@gmail.com>
2020-05-14 21:20:37 -0700

    finished Drill 3 <Roulette />

--- src/state-drills/Roulette.js
+++ src/state-drills/Roulette.js
@@ -11,12 +11,42 @@ class Roulette extends React.Component {
     }
     this.state.chamber = props.bulletInChamber
   }
+
+  toBeOrNotToBe() {
+    clearTimeout(this.timer)
+    const randomChamber = Math.ceil(Math.random() * this.props.bulletInChamber)
+    console.log(`Spinning Stopped at Chamber ${randomChamber}`)
+
+    if (randomChamber === this.state.chamber) {
+      this.setState({
+        content: 'BANG!! You\'re dead.',
+      })
+    } else {
+      this.setState({
+        content: 'Phew! You\'re safe.',
+        chamber: randomChamber,
+      })
+    }
+  }
+  
+  handleTrigger = () => {
+    console.log('bullet in chamber ' + this.state.chamber)
+    this.setState({
+      spinningTheChamber: true,
+      content: 'spinning the chamber and pulling the trigger! ...'
+    })
+
+    this.timer = setTimeout(() => {
+      this.toBeOrNotToBe()
+    }, 2000)
+
+  }
   
   render() {
     return (
       <div>
         <p>{this.state.content}</p>
-        <button>Pull the trigger!</button>
+        <button onClick={this.handleTrigger}>Pull the trigger!</button>
       </div>
     )
   }
